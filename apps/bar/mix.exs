0de081e2733d30ece8954c47f68b7d9a22def57d
Myron Marston <myron.marston@gmail.com>
2016-11-20 12:04:19 -0800

    Use ExCheck.

--- apps/bar/mix.exs
+++ apps/bar/mix.exs
@@ -35,6 +35,9 @@ defmodule Bar.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:excheck, "~> 0.5", only: :test},
+      {:triq, github: "triqng/triq", only: :test}
+    ]
   end
 end
--- apps/bar/test/bar_test.exs
+++ apps/bar/test/bar_test.exs
@@ -1,6 +1,10 @@
 defmodule BarTest do
   use ExUnit.Case
-  doctest Bar
+  use ExCheck
+
+  property :square do
+    for_all x in int, do: x * x >= 0
+  end
 
   test "the truth" do
     assert 1 + 1 == 2
